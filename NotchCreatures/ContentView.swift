f6e5415d10d63e88528b5e169b9f6044db4c2629
remon-nomer66 <c.lemon1366@gmail.com>
2024-07-30 21:15:07 +0900

    Initial Commit

---
+++ NotchCreatures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotchCreatures
+//
+//  Created by Remon Nakashima on 2024/07/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotchCreatures/NotchCreaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchCreaturesApp.swift
+//  NotchCreatures
+//
+//  Created by Remon Nakashima on 2024/07/30.
+//
+
+import SwiftUI
+
+@main
+struct NotchCreaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
